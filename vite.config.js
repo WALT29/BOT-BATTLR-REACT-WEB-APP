f624e89485609ddddd472cd50ad81d70c9af5e36
WALTER DAVID IRUNGU <daveirungu20@gmail.com>
2024-08-15 21:52:05 +0300

    deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/BOT-BATTLR-REACT-WEB-APP/"
+  base:"/BOT-BATTLR-REACT-WEB-APP/",
   plugins: [react()],
+  
 })
